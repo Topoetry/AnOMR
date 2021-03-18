ee8f2edaf9c1d131de5eca0a697a76ccdfb034c6
Topoetry <80897451+Topoetry@users.noreply.github.com>
2021-03-18 12:55:42 +0000

    Add files via upload

---
+++ AnOMR-polybar.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [[ -n $(AnOMR show|rg hybrid) ]]
+then
+	echo -e "%{F#808}"
+elif [[ -n $(AnOMR show|rg intel) ]]
+then
+	echo -e "%{F#037}"
+elif [[ -n $(AnOMR show|rg intel) ]]
+then
+	echo -e "%{F#073}"
+else
+	echo "error"
+fi
+
+
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cp -r etc/AnOMR /etc/
+
+cp AnOMR /usr/bin
+chmod 755 /usr/bin/AnOMR
+cp script/AnOMR-dmenu
+chmod 755 /usr/bin/AnOMR-dmenu
+echo "Look inside script directory for polybar module."
+cat runit/rc.local >> /etc/rc.local
+cat runit/rc.shutdown >> /etc/rc.shutdown
