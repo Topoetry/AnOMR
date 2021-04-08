885723d2c5b17dae8d93ce368dc1699f28d9044d
Topoetry <80897451+Topoetry@users.noreply.github.com>
2021-04-08 00:20:19 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cp -r etc/AnOMR /etc/
 
 cp AnOMR /usr/bin
 chmod 755 /usr/bin/AnOMR
-cp script/AnOMR-dmenu
+cp script/AnOMR-dmenu /usr/bin/
 chmod 755 /usr/bin/AnOMR-dmenu
 echo "Look inside script directory for polybar module."
 cat runit/rc.local >> /etc/rc.local
